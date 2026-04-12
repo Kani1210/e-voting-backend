55060587e4cdc7c2c7418ad5cdf03d2af22abf7c
kishorestunner <kishorevijay0010@gmail.com>
2026-04-12 14:19:38 +0530

    space increase for store biometric

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+// ✅ FIX 413 ERROR HERE
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.json({ message: "API Running ✔" });
