0f0e18e8736c9a163f374b74adc3a77b1d68de17
kishorestunner <kishorevijay0010@gmail.com>
2026-04-14 19:18:55 +0530

    update it

--- config/mail.js
+++ config/mail.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true only for port 465
+  secure: false,
   auth: {
-    user: "kishorevijay0010@gmail.com",
-    pass: "afub pmwb htbf yuce" // Gmail App Password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
   tls: {
     rejectUnauthorized: false,
