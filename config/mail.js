654ba18388b60d97d1407c4c941b0e2c7a5bd48d
kishorestunner <kishorevijay0010@gmail.com>
2026-04-14 19:54:32 +0530

    back to code in mail

--- config/mail.js
+++ config/mail.js
@@ -1,20 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 15000,
-  requireTLS: true,
-  tls: {
-    rejectUnauthorized: false,
-  },
-  logger: true,
-  debug: true,
+  connectionTimeout: 30000,
 });
-
-module.exports = transporter;
