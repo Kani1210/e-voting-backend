751e5e14ced9649b39efa53e7d82806857cbad59
kishorestunner <kishorevijay0010@gmail.com>
2026-04-14 19:42:42 +0530

    time increase for render

--- config/mail.js
+++ config/mail.js
@@ -2,15 +2,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 🔥 increase timeout
   tls: {
     rejectUnauthorized: false,
   },
 });
-
-module.exports = transporter;
